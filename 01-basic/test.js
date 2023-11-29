80930bae6c214248c775b55ac07891faba343fff
sidhanta08 <152358526+sidhanta08@users.noreply.github.com>
2023-11-29 04:26:29 +0000

    file added

---
+++ 01-basic/test.js
@@ -0,0 +1,6 @@
+console.log("hello  world");
+/*1.Javascript is a High level scripting Programming language used to developing web & mobile application effioiently.
+2.We use javascript in both client side as well as sever side language.
+3.We use javascript for developing logics in our code .
+4.javascript was developed in 1997.
+5.Now these days we used the Es6 version of javascript . it was started on 2015 and Now it's the lastest version of js.*/
