1416bd5806e6827034dfcd8ab45c8b61860fd8ff
sidhanta08 <152358526+sidhanta08@users.noreply.github.com>
2023-12-08 05:14:13 +0000

    add file

--- 01-basic/arrow.js
+++ 01-basic/arrow.js
@@ -3,3 +3,106 @@ arrow function - in es6 major update had done .it changes the old syntax of func
 - in arrow finction we need a variable to declare it and majorly used => this operator to define arrow function.
 */
 
+const user = {
+    username:"sid",
+    location:"ctc",
+    welcomemsg:function (){
+        console.log(`${this.username},good morning`);
+        console.log(this);
+
+    }
+}
+user.welcomemsg();
+user.username = "sidhu";
+user.welcomemsg();
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
